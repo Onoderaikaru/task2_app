7c12cfdcd709cc85e54505210a729a5db570a88e
Onoderaikaru <122001711+Onoderaikaru@users.noreply.github.com>
2023-01-14 00:42:09 +0900

    Update task2_app.py

--- task2_app.py
+++ task2_app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
-def HP_function(x):
+def my_function(x):
     return x[0] + 0.5*x[1] + 0.125*x[2] + 60
 
-def ABCDS_function(y):
+def my_function(y):
     return (y[0] + 0.5*y[1] + 0.125*y[2] + 5) * c
 
 st.title("実数値計算")
@@ -18,9 +18,6 @@ y2 = st.number_input("努力値(A,B,C,D,S):", value=0)
 
 c = st.sidebar.selectbox("性格補正",["1.1","1","0.9"])
 
-res1 = HP_function(x)
-res2 = ABCDS_function(y)
-
 st.write("実数値:")
-st.write("HP: ", res1)
-st.write("こうげき、ぼうぎょ、とくこう、とくぼう、すばやさ: ", res2)
+st.write("HP: ", my_function(x))
+st.write("こうげき、ぼうぎょ、とくこう、とくぼう、すばやさ: ", my_function(y))

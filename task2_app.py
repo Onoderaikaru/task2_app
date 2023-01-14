25f317791849f126821f402254bff6353529e9ce
Onoderaikaru <122001711+Onoderaikaru@users.noreply.github.com>
2023-01-14 11:24:41 +0900

    Update task2_app.py

--- task2_app.py
+++ task2_app.py
@@ -10,12 +10,12 @@ num4 = st.number_input("種族値(A,B,C,D,S):")
 num5 = st.number_input("個体値(A,B,C,D,S):")
 num6 = st.number_input("努力値(A,B,C,D,S):")
 
-c = st.sidebar.selectbox("性格補正",["1.1","1","0.9"])
+c = st.sidebar.selectbox("性格補正",["選択なし","1.1","1","0.9"])
 
-result1 = 0
-result2 = 0
-
-if c == "1.1":
+if c == "選択なし":
+  result1 = 0
+  result2 = 0
+elif c == "1.1":
   result1 = num1 + 0.5*num2 + 0.125*num3+ 60
   result2 = (num4 + 0.5*num5 + 0.125*num6 + 5) * 1.1
 elif c == "1.0":

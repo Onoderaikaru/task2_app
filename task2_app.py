933946b7f1f4209900eca469bf0d7d899ece88b3
Onoderaikaru <122001711+Onoderaikaru@users.noreply.github.com>
2023-01-14 11:11:52 +0900

    Update task2_app.py

--- task2_app.py
+++ task2_app.py
@@ -2,23 +2,23 @@ import streamlit as st
 
 st.title("実数値計算")
 
-A = st.number_input("種族値(HP):", value=0)
-B = st.number_input("個体値(HP):", value=0)
-C = st.number_input("努力値(HP):", value=0)
+num1 = st.number_input("種族値(HP):", value=0)
+num2 = st.number_input("個体値(HP):", value=0)
+num3= st.number_input("努力値(HP):", value=0)
 
-D = st.number_input("種族値(A,B,C,D,S):", value=0)
-E = st.number_input("個体値(A,B,C,D,S):", value=0)
-F = st.number_input("努力値(A,B,C,D,S):", value=0)
+num4 = st.number_input("種族値(A,B,C,D,S):", value=0)
+num5 = st.number_input("個体値(A,B,C,D,S):", value=0)
+num6 = st.number_input("努力値(A,B,C,D,S):", value=0)
 
 c = st.selectbox("性格補正",["1.1","1","0.9"])
 
 result = None
 if c == "1.1":
-  result1 = A + 0.5*B + 0.125*C+ 60, result2 = (D + 0.5*E + 0.125*F + 5) * 1.1
+  result1 = num1 + 0.5*num2 + 0.125*num3+ 60, result2 = (num4 + 0.5*num5 + 0.125*num6 + 5) * 1.1
 elif c == "1.0":
-  result1 = A + 0.5*B + 0.125*C+ 60, result2 = (D + 0.5*E + 0.125*F + 5) * 1.0
+  result1 = num1 + 0.5*num2 + 0.125*num3+ 60, result2 = (num4 + 0.5*num5 + 0.125*num6 + 5) * 1.0
 elif c == "0.9":
-  result1 = A + 0.5*B + 0.125*C+ 60, result2 = (D + 0.5*E + 0.125*F + 5) * 0.9
+  result1 = num1 + 0.5*num2 + 0.125*num3+ 60, result2 = (num4 + 0.5*num5 + 0.125*num6 + 5) * 0.9
 
 st.write("実数値:")
 st.write("HP: ", result1)

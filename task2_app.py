b5dc7f829890ea1e2960ab37e927228d695ee564
Onoderaikaru <122001711+Onoderaikaru@users.noreply.github.com>
2023-01-14 11:17:21 +0900

    Update task2_app.py

--- task2_app.py
+++ task2_app.py
@@ -1,20 +1,24 @@
 import streamlit as st
 
-st.title("My Calculator App")
+st.title("実数値計算")
 
-num1 = st.number_input("Enter a number:")
-num2 = st.number_input("Enter another number:")
+num1 = st.number_input("種族値(HP):")
+num2 = st.number_input("個体値(HP):")
+num3= st.number_input("努力値(HP):")
 
-operation = st.selectbox("Select an operation:", ["Addition", "Subtraction", "Multiplication", "Division"])
+num4 = st.number_input("種族値(A,B,C,D,S):")
+num5 = st.number_input("個体値(A,B,C,D,S):")
+num6 = st.number_input("努力値(A,B,C,D,S):")
+
+c = st.selectbox("性格補正",["1.1","1","0.9"])
 
 result = None
-if operation == "Addition":
-    result = num1 + num2
-elif operation == "Subtraction":
-    result = num1 - num2
-elif operation == "Multiplication":
-    result = num1 * num2
-elif operation == "Division":
-    result = num1 / num2
+if c == "1.1":
+  result1 = (num4 + 0.5*num5 + 0.125*num6 + 5) * 1.1
+elif c == "1.0":
+  result1 = (num4 + 0.5*num5 + 0.125*num6 + 5) * 1.0
+elif c == "0.9":
+  result1 = (num4 + 0.5*num5 + 0.125*num6 + 5) * 0.9
 
-st.write("The result is:", result)
+st.write("実数値:")
+st.write("HP: ", result1)

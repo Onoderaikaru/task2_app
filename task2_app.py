49785d9b5f547e3334da3f0d39f5522648c1bfae
Onoderaikaru <122001711+Onoderaikaru@users.noreply.github.com>
2023-01-14 11:04:23 +0900

    Update task2_app.py

--- task2_app.py
+++ task2_app.py
@@ -1,11 +1,5 @@
 import streamlit as st
 
-def my_function(x):
-  return x[0] + x[1]*0.5 + x[2]*0.125+ 60
-
-def my_function(y):
-  return (y[0] + y[1]*0.5 + y[2]*0.125 + 5) * c
-
 st.title("実数値計算")
 
 x0 = st.number_input("種族値(HP):", value=0)
@@ -16,11 +10,15 @@ y0 = st.number_input("種族値(A,B,C,D,S):", value=0)
 y1 = st.number_input("個体値(A,B,C,D,S):", value=0)
 y2 = st.number_input("努力値(A,B,C,D,S):", value=0)
 
-c = st.sidebar.selectbox("性格補正",["1.1","1","0.9"])
+c = st.selectbox("性格補正",["1.1","1","0.9"])
 
-res1 = my_function(x)
-res2 = my_function(y)
+if c == "1.1":
+  result1 = x[0] + 0.5*x[1] + 0.125*x[2]+ 60, result2 = (y[0] + 0.5*y[1] + 0.125*y[2] + 5) * 1.1
+elif c == "1.0":
+  result1 = x[0] + 0.5*x[1] + 0.125*x[2]+ 60, result2 = (y[0] + 0.5*y[1] + 0.125*y[2] + 5) * 1.0
+elif c == "0.9":
+  result1 = x[0] + 0.5*x[1] + 0.125*x[2]+ 60, result2 = (y[0] + 0.5*y[1] + 0.125*y[2] + 5) * 0.9
 
 st.write("実数値:")
-st.write("HP: ", res1.fun)
-st.write("こうげき、ぼうぎょ、とくこう、とくぼう、すばやさ: ", res2.fun)
+st.write("HP: ", result1)
+st.write("こうげき、ぼうぎょ、とくこう、とくぼう、すばやさ: ", result2)
